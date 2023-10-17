b22d48728f58c7c42657e8b1f200163ab0843ae7
MiloszRolewski <milosz.rolewski01@gmail.com>
2023-10-17 13:20:17 +0200

    .

--- lab02/B04.py
+++ lab02/B04.py
@@ -1,4 +1,3 @@
-from base64 import decode
 import re
 # mail_pattern=r'[\w\.-]+@[\w\.-]+'
 mail_pattern=r'\b[A-Z0-9a-z._%+-]+@[A-Z0-9a-z._-]+\.[A-Za-z]{2,}\b'
@@ -6,7 +5,7 @@ mail_pattern=r'\b[A-Z0-9a-z._%+-]+@[A-Z0-9a-z._-]+\.[A-Za-z]{2,}\b'
 
 #RSPO.html - https://rspo.gov.pl/institutions?q=%7B%22filter%22:%22wielkopolska%22%7D 
 # rządowa wyszukiwarka placówek oświatowych w wielkopolskce
-with open('RSPO.html','r',encoding='utf-8') as f_input:
+with open('RSPO.html','r',encoding='utf-8') as f_input: #encoding - problemy na windowsie z otworzeniem pliku
     with open('B04output.txt','w') as f_output:
         f_content = f_input.read()
         emails = re.findall(mail_pattern,f_content)
